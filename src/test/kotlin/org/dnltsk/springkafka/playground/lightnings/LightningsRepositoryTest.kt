8f1a1e3418ff4980ee362c14d8237befd1ce927b
danielt <daniel.teske@gmail.com>
2017-08-10 23:03:44 +0200

    LightningsRepositoryTest

---
+++ src/test/kotlin/org/dnltsk/springkafka/playground/lightnings/LightningsRepositoryTest.kt
@@ -0,0 +1,48 @@
+package org.dnltsk.springkafka.playground.lightnings
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.time.Instant
+
+class LightningsRepositoryTest {
+
+    @Mock
+    lateinit var validator: LightningValidator
+
+    @InjectMocks
+    lateinit var repository: LightningsRepository
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    val sampleLightning = Lightning(
+            occuredAt = Instant.parse("2017-08-10T20:27:42Z"),
+            type = LightningType.CLOUD_TO_CLOUD,
+            currentInAmpere = 8000.0,
+            location = Location(13.3888599, 52.5170365)
+    )
+
+    @Test
+    fun `repo starts empty`() {
+        val lightnings = repository.getLightnings()
+        assertThat(lightnings).isEmpty()
+    }
+
+    @Test
+    fun `added lightnigs are part are loadable`() {
+        repository.addLightning(sampleLightning.copy(occuredAt = Instant.parse("2016-01-01T00:00:00Z")))
+        repository.addLightning(sampleLightning.copy(occuredAt = Instant.parse("2017-01-01T00:00:00Z")))
+        repository.addLightning(sampleLightning.copy(occuredAt = Instant.parse("2018-01-01T00:00:00Z")))
+
+        val lightnings = repository.getLightnings()
+
+        assertThat(lightnings).hasSize(3)
+    }
+
+}
