951d4783016e9f8b196244e45d79285868c9e3bd
danielt <daniel.teske@gmail.com>
2017-08-11 01:21:50 +0200

    2h 59m old lightnings should be valid

--- src/test/kotlin/org/dnltsk/springkafka/playground/lightnings/LightningValidator_1_naiveTest.kt
+++ src/test/kotlin/org/dnltsk/springkafka/playground/lightnings/LightningValidator_1_naiveTest.kt
@@ -1,6 +1,7 @@
 package org.dnltsk.springkafka.playground.lightnings
 
 import org.assertj.core.api.Assertions.*
+import org.junit.Ignore
 import org.junit.Test
 import java.time.Duration
 import java.time.Instant
@@ -20,6 +21,23 @@ class LightningValidator_1_naiveTest {
     fun `1h old lightnings should be valid`() {
         val nowMinus1Hour = Instant.now().minus(Duration.ofHours(1))
         val validLightning = LIGHTNING_2016.copy(occuredAt = nowMinus1Hour)
+
+        assertThatCode { validator.validate(validLightning) }.doesNotThrowAnyException()
+    }
+
+    @Test @Ignore //ignored because we are 3h and some millis behind now :(
+    fun `3h old lightnings should be valid`() {
+        val nowMinus3Hour = Instant.now().minus(Duration.ofHours(3))
+        val validLightning = LIGHTNING_2016.copy(occuredAt = nowMinus3Hour)
+
+        assertThatCode { validator.validate(validLightning) }.doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `2h 59m old lightnings should be valid`() {
+        val nowMinus3Hour = Instant.now().minus(Duration.ofHours(3))
+        val nowMinus2h59m = nowMinus3Hour.plus(Duration.ofMinutes(1))
+        val validLightning = LIGHTNING_2016.copy(occuredAt = nowMinus2h59m)
 
         assertThatCode { validator.validate(validLightning) }.doesNotThrowAnyException()
     }
