396f27c7a364b2b5cd5df7833a8e28c48cd7f57c
danielt <daniel.teske@gmail.com>
2017-08-10 23:51:31 +0200

    IT -> WebTest

--- src/test/kotlin/org/dnltsk/springkafka/playground/lightnings/LightningsRestControllerWebTest.kt
+++ src/test/kotlin/org/dnltsk/springkafka/playground/lightnings/LightningsRestControllerWebTest.kt
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class LightningsRestControllerIT {
+class LightningsRestControllerWebTest {
 
     @Autowired
     lateinit var restTemplate: TestRestTemplate
