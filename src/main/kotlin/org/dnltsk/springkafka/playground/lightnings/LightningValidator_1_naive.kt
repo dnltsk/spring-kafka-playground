c0e88f7241144d5f31f558bf704a6996e097fe40
danielt <daniel.teske@gmail.com>
2017-08-11 01:23:04 +0200

    formatting

--- src/main/kotlin/org/dnltsk/springkafka/playground/lightnings/LightningValidator_1_naive.kt
+++ src/main/kotlin/org/dnltsk/springkafka/playground/lightnings/LightningValidator_1_naive.kt
@@ -9,7 +9,7 @@ class LightningValidator_1_naive {
 
     private val threeHours = Duration.ofHours(3)
 
-    fun validate(lightning: Lightning) {
+    fun validate(lightning: Lightning){
         val now = Instant.now()
         if (lightning.occuredAt.isBefore(now.minus(threeHours))) {
             throw IllegalArgumentException("lightning is too old")
